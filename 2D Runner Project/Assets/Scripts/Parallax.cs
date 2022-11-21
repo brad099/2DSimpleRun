db4f0203e511beed49ef75e6964e4763246f974a
B_r_A_d <brad990124@gmail.com>
2022-11-21 22:57:54 +0400

    patch4

--- 2D Runner Project/Assets/Scripts/Parallax.cs
+++ 2D Runner Project/Assets/Scripts/Parallax.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    public float _speed;
     [SerializeField] Renderer _backgroundRenderer;
+    private Transform _camera;
+    public Vector3 offset;
+    public float _speed;
     void Update()
     {
+        _camera= GameObject.FindGameObjectWithTag("MainCamera").transform;
         _backgroundRenderer.material.mainTextureOffset += new Vector2(_speed * Time.deltaTime,0);
+        transform.position = new Vector3(_camera.transform.position.x,0f + offset.y,0f + offset.z);
     }
 }
